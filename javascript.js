d397164693f0c07d928d327b69c7554fe05376a1
whatdislol <neunglert@gmail.com>
2024-05-21 16:11:50 +0700

    Add calculate, delete number, and operator functions

--- javascript.js
+++ javascript.js
@@ -1,7 +1,31 @@
 let firstNumber;
 let secondNumber;
 let operator;
-const error = "cringe"
+let currentValue = 0;
+const ERROR = "cringe"
+const numberButtons = document.querySelectorAll(".operand");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector(".clear");
+const togglePolarityButton = document.querySelector(".toggle-polarity");
+const percentButton = document.querySelector(".percentage");
+const deleteButton = document.querySelector(".delete");
+const decimalButton = document.querySelector(".decimal");
+const calculateButton = document.querySelector(".calculate");
+const equationDisplay = document.querySelector(".equation");
+const numberDisplay = document.querySelector(".current");
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => onNumberClicked(+button.textContent));
+});
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => onOperatorClicked(button.textContent));
+});
+calculateButton.addEventListener("click", calculate);
+deleteButton.addEventListener("click", deleteNumber);
+clearButton.addEventListener("click", clear);
+togglePolarityButton.addEventListener("click", togglePolarity);
+decimalButton.addEventListener("click", decimal);
+percentButton.addEventListener("click", percentage);
 
 function add(first, second){
     return first + second;
@@ -13,18 +37,69 @@ function multiply(first, second){
     return first * second;
 }
 function divide(first, second){
-    if(second === 0) return error;
+    if(second === 0) return ERROR;
     return first / second;
 }
 function operate(operator, first, second){
     switch(operator){
-        case "add":
+        case "+":
             return add(first, second);
-        case "subtract":
+        case "-":
             return subtract(first, second);
-        case "multiply":
+        case "*":
             return multiply(first, second);
-        case "subtract":
+        case "/":
             return divide(first, second);
     }
+}
+
+function onNumberClicked(number){
+    if(currentValue === 0){
+        numberDisplay.textContent = "";
+    }
+
+    numberDisplay.textContent += number;
+    currentValue = +numberDisplay.textContent;
+    if(operator){
+        secondNumber = currentValue;
+    } else {
+        firstNumber = currentValue;
+    }
+}
+
+function onOperatorClicked(operatorSelected){
+    operator = operatorSelected;
+    currentValue = 0;
+    numberDisplay.textContent = "0";
+}
+
+function calculate(){
+    let result = operate(operator, firstNumber, secondNumber);
+    firstNumber = result;
+    numberDisplay.textContent = result;
+    operator = "";
+}
+
+function clear(){
+    currentValue = 0;
+    numberDisplay.textContent = "0";
+}
+
+function deleteNumber(){
+    if(numberDisplay.textContent.length > 0){
+        numberDisplay.textContent = numberDisplay.textContent.slice(0, -1);
+        currentValue = +numberDisplay.textContent;
+    }
+}
+
+function togglePolarity(){
+
+}
+
+function decimal(){
+
+}
+
+function percentage(){
+    
 }
